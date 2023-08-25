e7ee3ae3b29ccf7c1c16027535ccb1c112bcba61
henrwang <>
2023-08-25 10:10:15 +0200

    quotes

--- jedi/jedi.py
+++ jedi/jedi.py
@@ -123,7 +123,7 @@ def jedi_printout(atoms,rim_list,delta_q,E_geometries, E_RIMs_total, proc_geom_R
             rim="dihedral"
             ind="%s%d %s%d %s%d %s%d"%(atoms.symbols[rim_list[i][0]],rim_list[i][0],atoms.symbols[rim_list[i][1]],rim_list[i][1],atoms.symbols[rim_list[i][2]],rim_list[i][2],atoms.symbols[rim_list[i][3]],rim_list[i][3])
             print('%6i%7s%-11s%30s%17.7f%9.1f%17.7f' % (i+1, " ", rim,ind, delta_q[i], proc_E_RIMs[i], E_RIMs[i]))
-
+    from . import quotes
 
 @jsonable('jedi')
 class Jedi:
