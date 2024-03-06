63f631c490f76f0db6b1a3dc06d62a067174c7bd
henrwang <128494206+henrwang@users.noreply.github.com>
2024-03-06 23:56:31 +0100

    test

--- tests/test_Jedi.py
+++ tests/test_Jedi.py
@@ -13,20 +13,20 @@ class  TestJEDIHCN():
     @classmethod
     def setup_class(cls):
 
-        mol=io.read('hcn/opt.json')
-        mol2=io.read('hcn/dis.json')
-        hessian=VibrationsData.read('hcn/modes.json')
+        mol=io.read('./tests/hcn/opt.json')
+        mol2=io.read('./tests/hcn/dis.json')
+        hessian=VibrationsData.read('./tests/hcn/modes.json')
         cls.hcn = Jedi(mol,mol2,hessian)
         cls.hcn.run()
-        phessian=VibrationsData.from_2d(mol,np.loadtxt('hcn/parthess'),indices=[2,3,5,8,9,11])
+        phessian=VibrationsData.from_2d(mol,np.loadtxt('./tests/hcn/parthess'),indices=[2,3,5,8,9,11])
         cls.phcn = Jedi(mol,mol2,phessian)
         cls.phcn.partial_analysis(indices=[2,3,5,8,9,11])
 
 
     def test_run(cls):
         '''Test if ERIMs are correct'''
-        assert np.array_equal(cls.hcn.E_RIMs,np.loadtxt('hcn/ERIMs'))
+        assert np.array_equal(cls.hcn.E_RIMs,np.loadtxt('./tests/hcn/ERIMs'))
     
     def test_partial(cls):
         '''Test if ERIMs are correct'''
-        assert np.array_equal(cls.phcn.E_RIMs,np.loadtxt('hcn/pERIMs'))
+        assert np.array_equal(cls.phcn.E_RIMs,np.loadtxt('./tests/hcn/pERIMs'))
