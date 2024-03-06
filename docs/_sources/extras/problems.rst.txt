d12e9ed5a8aaf48b05800bb682583a9bcc8c032b
henrwang <128494206+henrwang@users.noreply.github.com>
2024-03-06 22:35:34 +0100

    tests

---
+++ docs/_sources/extras/problems.rst.txt
@@ -0,0 +1,8 @@
+VASP
+====
+
+At the moment there is no get_vibrations() function for the ASE VASP calculator in the pip installable version. Instead you can use the developer's version of ASE from github:
+
+.. code-block:: console
+
+    $ git clone https://gitlab.com/ase/ase.git
---
+++ tests/test_Jedi.py
@@ -0,0 +1,29 @@
+import pytest
+from jedi.jedi import Jedi
+import ase.io as io
+from ase.vibrations.vibrations import VibrationsData
+import numpy as np
+
+
+
+class  TestJEDIHCN():
+  
+    @classmethod
+    def setup_class(cls):
+        mol=io.read('hcn/opt.json')
+        mol2=io.read('hcn/dis.json')
+        hessian=VibrationsData.read('hcn/modes.json')
+        cls.hcn = Jedi(mol,mol2,hessian)
+        cls.hcn.run()
+        phessian=VibrationsData.from_2d(mol,np.loadtxt('hcn/parthess'),indices=[2,3,5,8,9,11])
+        cls.phcn = Jedi(mol,mol2,phessian)
+        cls.phcn.partial_analysis(indices=[2,3,5,8,9,11])
+
+
+    def test_run(cls):
+        '''Test if ERIMs are correct'''
+        assert np.array_equal(cls.hcn.E_RIMs,np.loadtxt('hcn/ERIMs'))
+    
+    def test_partial(cls):
+        '''Test if ERIMs are correct'''
+        assert np.array_equal(cls.phcn.E_RIMs,np.loadtxt('hcn/pERIMs'))
