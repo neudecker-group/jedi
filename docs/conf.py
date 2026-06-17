2dfa1f0fe1fcc39b1cc500fc99a25e8348c8b0de
Luca Zeuch <l-zeuch@email.de>
2026-06-17 17:47:06 +0200

    docs: add the old JEDI logo
    Add back the JEDI logo currently visible on the live documentation;
    however, I have removed the static colour background in favour of
    transparency.
    
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>

--- docs/conf.py
+++ docs/conf.py
@@ -35,9 +35,11 @@ autodoc_default_options = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_logo = "_static/images/Logo_JEDI.png"
 html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "collapse_navigation": False,
+    "logo_only": True,
     "style_external_links": True,
 }
