d0f38c16b3e9cc6b93e7b204f2fcfe72345f52ca
Luca Zeuch <l-zeuch@email.de>
2026-06-19 17:26:45 +0200

    docs: conf: provide a :git: role for easy linking
    I'm very lazy, and other authors are probably as well. When wanting to
    link to a file in the repository, we can now write the following in
    reStructuredText:
    
        :git:`path/to/file`
    
    or, as a masked link:
    
        :git:`random text <path/to/file>`
    
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>

--- docs/conf.py
+++ docs/conf.py
@@ -7,10 +7,17 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import datetime
+from pathlib import Path
+from urllib.parse import quote
+
+from docutils import nodes
+from docutils.parsers.rst.roles import set_classes
+from sphinx.util.nodes import split_explicit_title
 
 project = "JEDI"
 author = "Neudecker Group"
 copyright = f"{datetime.datetime.now(tz=datetime.timezone.utc).date().year}, {author}"
+repository = "https://github.com/neudecker-group/jedi"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -18,10 +25,11 @@ copyright = f"{datetime.datetime.now(tz=datetime.timezone.utc).date().year}, {au
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.extlinks",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    "sphinx_rtd_theme",
+    #    "sphinx_rtd_theme",
 ]
 
 templates_path = ["_templates"]
@@ -41,4 +49,42 @@ html_static_path = ["_static"]
 html_theme = "sphinx_book_theme"
 html_theme_options = {
     "collapse_navigation": False,
+    "repository_url": repository,
+    "show_toc_level": 3,
+    "use_repository_button": True,
 }
+
+# -- Further customisation ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/development/tutorials/extending_syntax.html
+# https://docutils.sourceforge.io/docs/howto/rst-roles.html
+
+
+def git_role(role, rawtext, text, lineno, inliner, options=None, content=None):
+    """
+    Provide a :git:`path/to/file` shorthand, rendering as a link to the file on GitHub.
+    Also works as masked link: :git:`random text <path/to/file>`.
+
+    """
+    options = options or {}
+
+    env = inliner.document.settings.env
+    root = Path(env.srcdir).parent
+
+    _has_title, title, target = split_explicit_title(text)
+    title = title or target
+
+    path = root / target
+    if not path.exists():
+        msg = f"Broken link: {rawtext}: {path}"
+        err = inliner.reporter.error(msg, line=lineno)
+        return [inliner.problematic(rawtext, rawtext, err)], [err]
+
+    ref = f"{repository}/blob/main/{quote(target)}"
+
+    set_classes(options)
+    node = nodes.reference(rawtext, title, refuri=ref, **options)
+    return [node], []
+
+
+def setup(app):
+    app.add_role("git", git_role)
